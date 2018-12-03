8ed6f5e7d5b232d5262fbf9fe1a592a8bb01229d
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2018-12-03 20:40:33 +0200

    Add prettier and configure linter

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -8,8 +8,7 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
