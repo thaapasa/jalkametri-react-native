c0e01fc975c273299a7cc32b284c70ef292ba2c5
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2018-12-03 19:52:46 +0200

    Regenerate project files with lowercase name

--- app/android/app/src/main/java/com/jalkametri/MainActivity.java
+++ app/android/app/src/main/java/com/jalkametri/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "jAlkaMetri";
+        return "jalkametri";
     }
 }
